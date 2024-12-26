71a9551cac5377636ee93df224a4ad5234819731
h33n0k <valentin@gorr.in>
2024-12-26 16:37:07 +0100

    feat(archinstall): Add device setup

--- archinstall.sh
+++ archinstall.sh
@@ -65,23 +65,103 @@ handle_options() {
 		eval "$var='$value'"
 	}
 
-	[[ -z "$P_HOSTNAME" ]] && prompt "P_HOSTNAME" "hostname: "
-	[[ -z "$P_ROOT_PASSWORD" ]] && prompt "P_ROOT_PASSWORD" "root password: "
-	[[ -z "$P_USER" ]] && prompt "P_USER" "new user: "
-	[[ -z "$P_USER_PASSWORD" ]] && prompt "P_USER_PASSWORD" "password: "
-	[[ -z "$P_ZONE_INFO" ]] && echo "zone info: " && P_ZONE_INFO=$(find /usr/share/zoneinfo/ -type f | fzf --preview 'echo {} | cut -d/ -f5- | tr "/" " "' --height 40% --border --preview-window=down:1:wrap)
+	# [[ -z "$P_HOSTNAME" ]] && prompt "P_HOSTNAME" "hostname: "
+	# [[ -z "$P_ROOT_PASSWORD" ]] && prompt "P_ROOT_PASSWORD" "root password: "
+	# [[ -z "$P_USER" ]] && prompt "P_USER" "new user: "
+	# [[ -z "$P_USER_PASSWORD" ]] && prompt "P_USER_PASSWORD" "password: "
+	# [[ -z "$P_ZONE_INFO" ]] && echo "zone info: " && P_ZONE_INFO=$(find /usr/share/zoneinfo/ -type f | fzf --preview 'echo {} | cut -d/ -f5- | tr "/" " "' --height 40% --border --preview-window=down:1:wrap)
 	[[ -z "$P_DEVICE" ]] && echo "device: " && P_DEVICE=$(lsblk -o NAME,SIZE,TYPE | grep disk | awk '{print "/dev/" $1 " (" $2 ")"}' | fzf --prompt="Select a device: " --height 40% --border | awk '{print $1}')
-	[[ -z "$P_LOCALE" ]] && echo "locale: " && P_LOCALE=$(grep -E '^.*UTF-8' /etc/locale.gen | awk '{print $1}' | fzf --preview 'echo {}' --height 40% --border --preview-window=down:3:wrap)
+	# [[ -z "$P_LOCALE" ]] && echo "locale: " && P_LOCALE=$(grep -E '^.*UTF-8' /etc/locale.gen | awk '{print $1}' | fzf --preview 'echo {}' --height 40% --border --preview-window=down:3:wrap)
 }
 
-# Call the function to handle options
 handle_options "$@"
 
-echo "hostname: $P_HOSTNAME"
-echo "zone_info: $P_ZONE_INFO"
-echo "locale: $P_LOCALE"
-echo "device: $P_DEVICE"
-echo "root_password: $P_ROOT_PASSWORD"
-echo "user: $P_USER"
-echo "user_password: $P_USER_PASSWORD"
-echo "encrypt: $P_ENCRYPT"
+# Partitioning disks
+gdisk "$P_DEVICE" <<EOF
+o
+Y
+n
+1
+
++512M
+ef00
+n
+2
+
++1G
+ef02
+n
+3
+
+
+$( [[ "$P_ENCRYPT" == true ]] && echo 8309 || echo 8300 )
+w
+y
+EOF
+
+# Define partitions
+EFI_PARTITION=$(lsblk -lnpo NAME "$P_DEVICE" | grep -E '1$' | tail -n 1)
+BOOT_PARTITION=$(lsblk -lnpo NAME "$P_DEVICE" | grep -E '2$' | tail -n 1)
+LVM_PARTITION=$(lsblk -lnpo NAME "$P_DEVICE" | grep -E '3$' | tail -n 1)
+SWAP_PARTITION=""
+HOME_PARTITION=""
+ROOT_PARTITION=""
+
+if [[ "$P_ENCRYPT" == true ]]; then
+	# Load encryption modules
+	modprobe dm-crypt
+	modprobe dm-mod
+
+	# Encrypt partition
+	cryptsetup luksFormat -v -s 512 -h sha512 "$LVM_PARTITION"	# Encrypt
+	cryptsetup open "$LVM_PARTITION" arch-lvm										# Open
+
+	# LVM partitioning
+	pvcreate /dev/mapper/arch-lvm       # Create physical volume
+	vgcreate arch /dev/mapper/arch-lvm  # Create volume group
+
+	HOME_PARTITION="/dev/mapper/arch-home"
+	ROOT_PARTITION="/dev/mapper/arch-root"
+	SWAP_PARTITION="/dev/mapper/arch-swap"
+else
+	pvcreate "$LVM_PARTITION"				# Create physical volume
+	vgcreate arch "$LVM_PARTITION"	# Create volume group
+
+	HOME_PARTITION="/dev/arch/home"
+	ROOT_PARTITION="/dev/arch/root"
+	SWAP_PARTITION="/dev/arch/swap"
+fi
+
+TOTAL_SIZE=$(lsblk -o SIZE -n "$LVM_PARTITION" | head -n 1 | tr -d '[:space:]G')
+
+compute_size() {
+	fr=$(printf "%.2f\n" $(echo "$TOTAL_SIZE / 10" | bc -l))
+	printf "%.2fG\n" $(echo "$fr * $1" | bc -l)
+}
+
+SWAP_PARTITION_SIZE="$(compute_size 1)"
+ROOT_PARTITION_SIZE="$(compute_size 4)"
+
+# Create logical volumes
+lvcreate -n swap -L "$SWAP_PARTITION_SIZE" -C y arch  # SWAP
+lvcreate -n root -L "$ROOT_PARTITION_SIZE" -C y arch  # ROOT
+lvcreate -n home -l +100%FREE arch										# HOME
+
+# FS formatting
+mkfs.fat -F32 "$EFI_PARTITION"				# EFI
+mkfs.ext4 "$BOOT_PARTITION"						# BOOT
+mkfs.btrfs -L root "$ROOT_PARTITION"	# ROOT
+mkfs.btrfs -L home "$HOME_PARTITION"	# HOME
+mkswap "$SWAP_PARTITION"							# SWAP
+
+# Partitions mounting
+## SWAP
+swapon "$SWAP_PARTITION"
+swapon -a
+
+mount "$ROOT_PARTITION" /mnt					# ROOT
+mkdir -p /mnt/{home,boot}
+mount "$BOOT_PARTITION" /mnt/boot			# BOOT
+mkdir /mnt/boot/efi
+mount "$EFI_PARTITION" /mnt/boot/efi	# EFI
+mount "$HOME_PARTITION" /mnt/home			# HOME
